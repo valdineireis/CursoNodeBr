1a257d460f8536c101955f59ee12ef00f8304e60
Valdinei Reis <valdineimtz@gmail.com>
2018-12-18 00:12:38 -0200

    Manipulando arquivos - Delete

--- modulo-04/test.js
+++ modulo-04/test.js
@@ -29,4 +29,10 @@ describe('Suite de manipulação de Herois', () => {
 
         deepEqual(actual, expected)
     })
+
+    it('deve remover um heroi por id', async () => {
+        const expected = true;
+        const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id);
+        deepEqual(resultado, expected)
+    })
 })
