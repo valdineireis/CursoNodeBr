8c8b1edea937b81e4d08798680f191b030b0b588
Valdinei Reis <valdineimtz@gmail.com>
2018-12-20 01:35:13 -0200

    Manipulação de arquivos - UPDATE

--- modulo-04/database.js
+++ modulo-04/database.js
@@ -81,6 +81,25 @@ class Database {
         dados.splice(indice, 1)
         return await this.escreverArquivo(dados)
     }
+
+    async atualizar(id, modificacoes) {
+        const dados = await this.obterDadosArquivo()
+        const indice = dados.findIndex(item => item.id === parseInt(id))
+        if (indice === -1) {
+            throw Error('O heroi informado nao existe')
+        }
+        const atual = dados[indice]
+        const objetoAtualizar = {
+            ...atual,
+            ...modificacoes
+        }
+        dados.splice(indice, 1)
+
+        return await this.escreverArquivo([
+            ...dados,
+            objetoAtualizar
+        ])
+    }
 }
 
 module.exports = new Database()
--- modulo-04/test.js
+++ modulo-04/test.js
@@ -11,9 +11,17 @@ const DEFAULT_ITEM_CADASTRAR = {
     id: 1
 }
 
+const DEFAULT_ITEM_ATUALIZAR = {
+    nome: 'Lanterna Verde',
+    poder: 'Energia do Anel',
+    id: 2
+}
+
 describe('Suite de manipulação de Herois', () => {
     before(async () => {
+        await database.remover();
         await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        await database.cadastrar(DEFAULT_ITEM_ATUALIZAR)
     })
 
     it('deve pesquisar um heroi usando arquivos', async () => {
@@ -33,6 +41,21 @@ describe('Suite de manipulação de Herois', () => {
     it('deve remover um heroi por id', async () => {
         const expected = true;
         const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id);
+        deepEqual(resultado, expected)
+    })
+
+    it('deve atualizar um heroi pelo id', async () => {
+        const expected = {
+            ...DEFAULT_ITEM_ATUALIZAR,
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        }
+        const novoDato = {
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        }
+        await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id, novoDato)
+        const [resultado] = await database.listar(DEFAULT_ITEM_ATUALIZAR.id)
         deepEqual(resultado, expected)
     })
 })
